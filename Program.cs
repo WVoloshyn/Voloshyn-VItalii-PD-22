374379d7a167764e7a712b9f7de7a963620b144e
WVoloshyn <71668172+WVoloshyn@users.noreply.github.com>
2020-12-07 12:47:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//6 variant//
+namespace lab1
+{
+    class  program
+    {.
+        static void Main(string[] args)
+    {
+        
+            Console.WriteLine("Висота=");
+                int high = Convert.ToInt32(Console.ReadLine()); ;
+        Console.WriteLine("Радіус=");
+                int radius = Convert.ToInt32(Console.ReadLine()); ;
+            int V;
+            V = pi * sqrt(radius) * high;
+            Console.WriteLine(V);
+        }
+    }
+}

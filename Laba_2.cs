811e2dbc41a6a02f5c5832dc2d864ee2e713cb49
WVoloshyn <71668172+WVoloshyn@users.noreply.github.com>
2020-12-13 14:51:47 +0200

    Add files via upload

---
+++ Laba_2.cs
@@ -0,0 +1,31 @@
+﻿//Variant 6
+using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn, nk;
+            do {
+                Console.Write("Enter nn: ");
+                nn = Convert.ToInt32(Console.ReadLine());
+            } while (nn<0);
+            do
+            {
+                Console.Write("Enter nk: ");
+                nk = Convert.ToInt32(Console.ReadLine());
+            } while (nk<nn);
+
+            double product = 1;
+            for (int k = nn; k <= nk; k++)
+            {
+                product = product * (Convert.ToInt32((Math.Pow((-1),k*k+1))*k*k-2)/3*k*k-2*k);
+            }
+
+            Console.WriteLine("Product = {0}", product);
+
+        }
+    }
+}

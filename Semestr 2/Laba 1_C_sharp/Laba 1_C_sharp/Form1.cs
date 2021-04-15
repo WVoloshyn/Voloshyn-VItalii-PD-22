43e31a49ddeb11d8ede8d531148bdabe3b4610d7
WVoloshyn <71668172+WVoloshyn@users.noreply.github.com>
2021-04-15 20:32:06 +0300

    Add files via upload

---
+++ Semestr 2/Laba 1_C_sharp/Laba 1_C_sharp/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_1_C_sharp
+{
+    public partial class Form1 : Form
+    {
+        Caclulator calc = new Caclulator();
+        public Form1()
+        {
+            InitializeComponent();
+            calc.CreateNumberField(this);
+            calc.Display(calc.buttonsList, this);
+            this.Size = new Size(366, 367);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.White;
+            this.Text = "TheBestCalculatorEver";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
